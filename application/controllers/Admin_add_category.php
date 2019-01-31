21d4cb291c0b296461de2bd5b3dcbb4a90df5eb8
Sujit Sarkar <sweezit92@gmail.com>
2019-01-31 17:53:11 +0530

    Update Admin_add_category.php

--- application/controllers/Admin_add_category.php
+++ application/controllers/Admin_add_category.php
@@ -20,27 +20,28 @@ class Admin_add_category extends CI_Controller {
 		$this->load->model('admin_add_category_m');
 		$cat_name = $this->input->post('cat_name');
 		$date = time();
-		if(!empty($_FILES['image']['name'])){
-			$config['upload_path'] = 'uploads/';
-			$config['allowed_types'] = 'jpg|jpeg|png|gif';
-			$config['file_name'] = rand(999,99999).$_FILES['image']['name'];
-			
-			$this->load->library('upload',$config);
-			$this->upload->initialize($config);
-			
-			if($this->upload->do_upload('image')){
-				$uploadData = $this->upload->data();
-				$image = $uploadData['file_name'];
-			}else{
-				$image = '';		
-			}
-		}else{
-			$image = '';		
-		}
+		
 		
 		$check_duplicate = $this->admin_add_category_m->check_duplicate($cat_name);
 		if($check_duplicate < 1)
 		{
+			if(!empty($_FILES['image']['name'])){
+				$config['upload_path'] = 'uploads/';
+				$config['allowed_types'] = 'jpg|jpeg|png|gif';
+				$config['file_name'] = rand(999,99999).$_FILES['image']['name'];
+				
+				$this->load->library('upload',$config);
+				$this->upload->initialize($config);
+				
+				if($this->upload->do_upload('image')){
+					$uploadData = $this->upload->data();
+					$image = $uploadData['file_name'];
+				}else{
+					$image = '';		
+				}
+			}else{
+				$image = '';		
+			}
 			$insert_array = array('category_name' => $cat_name,'category_image' => $image,'category_date' => $date);
 			$insert_query = $this->admin_add_category_m->insert_query($insert_array);
 			$this->session->set_flashdata("success", "Category inserted successfully!");
