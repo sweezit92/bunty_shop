985c07e7943a19b6762d0ffeaee90991624e90b6
Sujit Sarkar <sweezit92@gmail.com>
2019-01-31 17:54:45 +0530

    Update Admin_edit_category.php

--- application/controllers/Admin_edit_category.php
+++ application/controllers/Admin_edit_category.php
@@ -24,31 +24,31 @@ class Admin_edit_category extends CI_Controller {
 		$cat_id = $this->uri->segment(3);
 		$cat_name = $this->input->post('cat_name');
 		
-		if(!empty($_FILES['image']['name'])){
-			$config['upload_path'] = 'uploads/';
-			$config['allowed_types'] = 'jpg|jpeg|png|gif';
-			$config['file_name'] = rand(999,99999).$_FILES['image']['name'];
-			
-			$this->load->library('upload',$config);
-			$this->upload->initialize($config);
-			
-			if($this->upload->do_upload('image')){
-				$uploadData = $this->upload->data();
-				$image = $uploadData['file_name'];
-				$img_name = $this->input->post('img_name');
-				$update_array = array('category_name' => $cat_name,'category_image' => $image);
+		
+		$check_duplicate = $this->admin_edit_category_m->check_duplicate($cat_name,$cat_id);
+		if($check_duplicate < 1)
+		{
+			if(!empty($_FILES['image']['name'])){
+				$config['upload_path'] = 'uploads/';
+				$config['allowed_types'] = 'jpg|jpeg|png|gif';
+				$config['file_name'] = rand(999,99999).$_FILES['image']['name'];
+				
+				$this->load->library('upload',$config);
+				$this->upload->initialize($config);
+				
+				if($this->upload->do_upload('image')){
+					$uploadData = $this->upload->data();
+					$image = $uploadData['file_name'];
+					$img_name = $this->input->post('img_name');
+					$update_array = array('category_name' => $cat_name,'category_image' => $image);
+				}else{
+					$img_name = '';
+					$update_array = array('category_name' => $cat_name);
+				}
 			}else{
 				$img_name = '';
 				$update_array = array('category_name' => $cat_name);
 			}
-		}else{
-			$img_name = '';
-			$update_array = array('category_name' => $cat_name);
-		}
-		$check_duplicate = $this->admin_edit_category_m->check_duplicate($cat_name,$cat_id);
-		if($check_duplicate < 1)
-		{
-			
 			$update_query = $this->admin_edit_category_m->update_query($update_array,$cat_id,$img_name);
 			$this->session->set_flashdata("success", "Category updated successfully!");
 	        redirect('admin_edit_category/'.$cat_id);
